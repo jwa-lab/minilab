b5f6b3c8b71edcc4a74739000a545a4d7d763f36
Olivier Scherrer <pode.fr@gmail.com>
2021-01-29 00:28:38 -0500

    Create CLI for minilab

---
+++ src/banner.js
@@ -0,0 +1,16 @@
+const chalk = require('chalk');
+const figlet = require('figlet');
+const gradient = require('gradient-string');
+const package = require('../package.json');
+
+module.exports = {
+    showBanner() {
+        console.log(
+            chalk`${ gradient.pastel(figlet.textSync('minilab', { 
+                font: 'DOS Rebel'
+              })) }
+                                                 ${ package.author } - v${ package.version }
+`
+          );
+    }
+}
