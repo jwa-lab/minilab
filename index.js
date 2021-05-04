69ffbc6050d85f51636b935c66bcf61ff563197a
Olivier Scherrer <pode.fr@gmail.com>
2021-05-03 22:39:04 -0400

    BSP-18: Update Airlock
    - Use Airlock 2, switch from gRPC to REST
    - Add pull command to force update
    - Update minilab to expose port 8000 for HTTP

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
-const { Command } = require('commander');
-const clear = require('clear');
-const compose = require('docker-compose');
-const path = require('path');
+const { Command } = require("commander");
+const clear = require("clear");
+const compose = require("docker-compose");
+const path = require("path");
 
-const package = require('./package.json');
-const { showBanner }  = require('./src/banner');
+const package = require("./package.json");
+const { showBanner } = require("./src/banner");
 
 clear();
 
@@ -15,42 +15,65 @@ showBanner();
 const program = new Command();
 
 program
-    .description('minilab start a JWA Platform development environment')
-    .version(package.version)
-    .name('minilab')
-    .usage("[global options] command");
+  .description("minilab start a JWA Platform development environment")
+  .version(package.version)
+  .name("minilab")
+  .usage("[global options] command");
 
 program
-    .command('start')
-    .description('start the minilab environment')
-    .action(() => {
-        compose.upAll({ cwd: path.join(__dirname), log: true })
-            .then(
-                () => { console.log('minilab started')},
-                err => { console.log('something went wrong:', err.message)}
-            );
-    });
+  .command("start")
+  .description("start the minilab environment")
+  .action(() => {
+    compose.upAll({ cwd: path.join(__dirname), log: true }).then(
+      () => {
+        console.log("minilab started");
+      },
+      (err) => {
+        console.log("something went wrong:", err.message);
+      }
+    );
+  });
 
 program
-    .command('stop')
-    .description('stop the minilab environment')
-    .action(() => {
-        compose.down({ cwd: path.join(__dirname), log: true })
-            .then(
-                () => { console.log('minilab stopped')},
-                err => { console.log('something went wrong:', err.message)}
-            );
-    });
+  .command("pull")
+  .description("download the latest versions of the minilab environment")
+  .action(() => {
+    compose.upAll({ cwd: path.join(__dirname), log: true }).then(
+      () => {
+        console.log("minilab started");
+      },
+      (err) => {
+        console.log("something went wrong:", err.message);
+      }
+    );
+  });
 
 program
-    .command('kill')
-    .description('kill the minilab environment')
-    .action(() => {
-        compose.kill({ cwd: path.join(__dirname), log: true })
-            .then(
-                () => { console.log('minilab killed')},
-                err => { console.log('something went wrong:', err.message)}
-            );
-    })
+  .command("stop")
+  .description("stop the minilab environment")
+  .action(() => {
+    compose.down({ cwd: path.join(__dirname), log: true }).then(
+      () => {
+        console.log("minilab stopped");
+      },
+      (err) => {
+        console.log("something went wrong:", err.message);
+      }
+    );
+  });
+
+program
+  .command("kill")
+  .description("kill the minilab environment")
+  .action(() => {
+    compose.kill({ cwd: path.join(__dirname), log: true }).then(
+      () => {
+        console.log("minilab killed");
+      },
+      (err) => {
+        console.log("something went wrong:", err.message);
+      }
+    );
+  });
 
 program.parse(process.argv);
